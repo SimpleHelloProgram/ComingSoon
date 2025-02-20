e1e53553297dab6ee05105ccd3e61df91fcbd7a6
SimpleHelloProgram <130428450+SimpleHelloProgram@users.noreply.github.com>
2025-02-20 15:30:56 -0800

    First add

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ComingSoon from "./components/comingSoon";
+
+function App() {
+  return (
+    <>
+      <ComingSoon />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/comingSoon.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import {
+  faInstagram,
+  faFacebook,
+  faPinterest,
+} from "@fortawesome/free-brands-svg-icons";
+
+const ComingSoon = () => {
+  return (
+    <div className="flex flex-col h-screen w-screen bg-gradient-to-b from-gray-400 to-gray-900 justify-center items-center p-4 font-[Saira]">
+      <div className="flex flex-col items-center text-center mx-auto w-full">
+        <h1 className="text-white text-[200px] font-bold mb-8 font-serif text-center">
+          Coming Soon
+        </h1>
+        <p className="text-gray-300 text-[40px] mb-12 text-center">
+          We&apos;re working hard to bring you something amazing. Stay tuned!
+        </p>
+
+        <p className="text-gray-300 text-[40px] mb-12 text-center">
+          Follow our Socials to stay updated
+        </p>
+
+        <div className="flex flex-row gap-2 text-[29px] ">
+          <a
+            href="https://www.instagram.com/yourprofile"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center text-white px-8 py-3 rounded-lg transition duration-300 mb-4"
+          >
+            <FontAwesomeIcon icon={faInstagram} className="mr-2" />
+          </a>
+          <a
+            href="https://www.facebook.com/yourprofile"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center text-white px-8 py-3 rounded-lg transition duration-300 mb-4"
+          >
+            <FontAwesomeIcon icon={faFacebook} className="mr-2" />
+          </a>
+          <a
+            href="https://www.pinterest.com/yourprofile"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center text-white px-8 py-3 rounded-lg transition duration-300 mb-4"
+          >
+            <FontAwesomeIcon icon={faPinterest} className="mr-2" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ComingSoon;
